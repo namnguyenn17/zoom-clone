de18a29a7643c3ef7baba28170b74ad7e2ec07d9
Nam Nguyen <ducnam1719@gmail.com>
2024-04-03 05:37:06 +0700

    feat: init project & sidebar

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    label: 'Home',
+    routes: '/',
+    imgUrl: '/icons/home.svg',
+  },
+  {
+    label: 'Upcoming',
+    routes: '/upcoming',
+    imgUrl: '/icons/upcoming.svg',
+  },
+  {
+    label: 'Previous',
+    routes: '/previous',
+    imgUrl: '/icons/previous.svg',
+  },
+  {
+    label: 'Recordings',
+    routes: '/recordings',
+    imgUrl: '/icons/Video.svg',
+  },
+  {
+    label: 'Personal Room',
+    routes: '/personal-room',
+    imgUrl: '/icons/add-personal.svg',
+  },
+]
